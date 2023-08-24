7cf7d3ffe23e6e79e2676791346f3771b4ae8992
myndsol-anuragthapa <anurag.thapa@myndsol.com>
2023-08-24 10:57:43 +0530

    Adding configuration

---
+++ src/CompleteDeployment/config/configuration.py
@@ -0,0 +1,8 @@
+from CompleteDeployment.constants import *
+import os
+from pathlib import Path
+from CompleteDeployment.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig,
+                                                EvaluationConfig)
