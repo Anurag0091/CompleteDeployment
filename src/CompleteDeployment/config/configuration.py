8496e09203df4f335dc8f293d9da072be1f61229
myndsol-anuragthapa <anurag.thapa@myndsol.com>
2023-08-24 15:06:17 +0530

    Creating components

--- src/CompleteDeployment/config/configuration.py
+++ src/CompleteDeployment/config/configuration.py
@@ -1,8 +1,30 @@
 from CompleteDeployment.constants import *
 import os
+from CompleteDeployment.utils.common import create_directories,read_yaml
 from pathlib import Path
+
+
 from CompleteDeployment.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
                                                 TrainingConfig,
                                                 EvaluationConfig)
+
+class ConfigurationManager:
+    def __init__(
+            self,
+            config_filepath=CONFIG_FILE_PATH,
+            params_filepath= PARAMS_FILE_PATH):
+            
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self)-> DataIngestionConfig:
+        config= self.config.data_ingestion
+        create_directories([config.root_dir])
+        data_ingestion_config=DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir
+        )
+        return data_ingestion_config
---
+++ src/CompleteDeployment/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,6 @@
+
+from CompleteDeployment.config.configuration import ConfigurationManager
+from CompleteDeployment.components.data_ingestion import DataIngestion
+from CompleteDeployment import logger
+
+STAGE_NAME = "Data Ingestion stage"
